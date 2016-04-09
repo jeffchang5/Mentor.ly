5a32c3c714f5c283a6fe4dfa534d4a39846267d7
Jeffrey Chang <chang.jeffrey25@gmail.com>
2016-04-09 18:22:35 -0400

    First commit

--- Mentor.ly/SignupController.swift
+++ Mentor.ly/SignupController.swift
@@ -1,18 +1,19 @@
 //
-//  ViewController.swift
+//  SignupController.swift
 //  Mentor.ly
 //
-//  Created by Jeffrey Chang on 4/4/16.
+//  Created by Jeffrey Chang on 4/9/16.
 //  Copyright © 2016 Jeffrey Chang. All rights reserved.
 //
 
+import Foundation
 import UIKit
-
-class ViewController: UIViewController {
-    
+class SignupController: ViewController {
+    @IBOutlet weak var username: UITextField!
     override func viewDidLoad() {
+
         super.viewDidLoad()
-        
+
         // Do any additional setup after loading the view, typically from a nib.
     }
     
@@ -20,7 +21,8 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-    
+    @IBAction func onSubmit(sender: UIButton) {
+        username.text
+    }
     
 }
-
